6a1a35a8ef989e2ef4891287f1a4c099a7a0ad77
Maeva D <delruemaeva@orange.fr>
2024-12-17 18:32:24 +0100

    check value of return_date before making a new rental

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Get, Patch, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Body,
+  Param,
+  BadRequestException,
+} from '@nestjs/common';
 import { CustomerService } from './customers/customer.service';
 import { RentalService } from './rentals/rental.service';
 import {
@@ -73,6 +81,22 @@ export class AppController {
     },
   ): Promise<RentalModel> {
     const { rental_date, return_date, customer, inventory, staff } = rentalData;
+
+    const returnDate = new Date(return_date);
+
+    const oneWeekLater = new Date(rental_date);
+    oneWeekLater.setDate(oneWeekLater.getDate() + 7);
+    // oneWeekLater.setHours(0, 0, 0, 0); // Définir l'heure à partir de minuit
+
+    const threeWeeksLater = new Date(rental_date);
+    threeWeeksLater.setDate(threeWeeksLater.getDate() + 21);
+    // threeWeeksLater.setHours(23, 59, 59, 0); // Définir l'heure juste avant minuit
+
+    if (returnDate < oneWeekLater || returnDate > threeWeeksLater) {
+      throw new BadRequestException(
+        'La date de retour doit être supérieure à une semaine et inférieure à trois semaines à partir de la date de la location',
+      );
+    }
 
     return this.rentalService.createRental({
       rental_date,
